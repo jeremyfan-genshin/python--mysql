bd2487c98053d52d0f0fe3cd56643da85449d773
Jeremy <jeremyfan.genshin@gmail.com>
2025-06-24 14:48:49 +0800

    修改dbuser權限，讀取資料庫內容

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 #
 print("建立資料庫連線中")
-mysql.connector.connect(
+cnx=mysql.connector.connect(
     host="127.0.0.1",
     port=3306,
     user="dbuser",
@@ -10,3 +10,6 @@ mysql.connector.connect(
 
 )
 print("透過連線取得 cursor物件")
+dbcursor=cnx.cursor()
+print("執行 select name from city")
+dbcursor.execute("select name from world.city")
